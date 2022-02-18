cf67d63c388490dbed6316e42cacf05ae15ce249
RafaellaCristinaReW <rafacssacrament@gmail.com>
2022-02-18 16:58:04 -0300

    teste

---
+++ echo_print.php
@@ -0,0 +1,16 @@
+<!DOCKTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<title> </title>
+		</head>
+
+		<body>
+			<?php 
+				echo 'Comando<br/>'; 
+				print 'Comando<br/>';
+			?>
+			teste
+
+		</body>
+	</html>
